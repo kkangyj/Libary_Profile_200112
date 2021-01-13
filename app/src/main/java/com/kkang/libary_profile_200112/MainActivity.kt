8f9ac31872b3576724acfb3bb0071d3766f55c94
kkangyun <kkang_yj36@naver.com>
2021-01-13 15:50:53 +0900

    프로필 사진 누르면 사진크게 보는 화면으로 이동   + 사진 보이는 화면 액션바 제거

--- app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
+++ app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.kkang.libary_profile_200112
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        profilePhotoImg.setOnClickListener {
+
+            val myIntent = Intent(mContext, ViewProfilePhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
---
+++ app/src/main/java/com/kkang/libary_profile_200112/ViewProfilePhotoActivity.kt
@@ -0,0 +1,11 @@
+package com.kkang.libary_profile_200112
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewProfilePhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_profile_photo)
+    }
+}
