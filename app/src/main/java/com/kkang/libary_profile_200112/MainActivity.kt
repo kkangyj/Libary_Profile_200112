d3e722aabe8d2869d3d433d26ae9a1721735d1b7
kkangyun <kkang_yj36@naver.com>
2021-02-17 16:14:08 +0900

    전화걸기 권한 허용시에만 전화걸기.

--- app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
+++ app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.kkang.libary_profile_200112
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -21,6 +25,25 @@ class MainActivity : BaseActivity() {
             val myIntent = Intent(mContext, ViewProfilePhotoActivity::class.java)
             startActivity(myIntent)
 
+        }
+
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:010-1234-1234")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "권한을 거부하면 통화가 불가능합니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.with(mContext).setPermissionListener(pl)
+                .setDeniedMessage("'설정 > 권한' 에서 전화걸기 허용을 해주세요.")
+                .setPermissions(android.Manifest.permission.CALL_PHONE).check()
         }
 
     }
