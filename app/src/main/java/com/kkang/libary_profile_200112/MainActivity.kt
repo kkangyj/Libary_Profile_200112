3ca85d4cad41ea64430d640b43b6d9f3e14a2ae9
kkangyun <kkang_yj36@naver.com>
2021-02-17 14:54:15 +0900

    Glide 적용해서 사진 로드

--- app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
+++ app/src/main/java/com/kkang/libary_profile_200112/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kkang.libary_profile_200112
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -25,6 +26,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        Glide.with(mContext)
+            .load("https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F9956CB395DEA3BCA0D")
+            .into(profilePhotoImg)
 
     }
 }
